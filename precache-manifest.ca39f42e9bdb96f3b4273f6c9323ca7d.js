3bb8a6d4a610a6ec66bd803f1df012889c4660e4
Manolingam <manolingam1996@gmail.com>
2020-04-12 21:07:32 +0530

    Updates

---
+++ precache-manifest.ca39f42e9bdb96f3b4273f6c9323ca7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9d402c09c3d83aaabb0ebbac2bda3db",
+    "url": "/covid19-india-stats/index.html"
+  },
+  {
+    "revision": "ae53feb769f01975c781",
+    "url": "/covid19-india-stats/static/css/main.bd78d2a9.chunk.css"
+  },
+  {
+    "revision": "2d8d2eb0d0cbf962c456",
+    "url": "/covid19-india-stats/static/js/2.73ea4151.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/covid19-india-stats/static/js/2.73ea4151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae53feb769f01975c781",
+    "url": "/covid19-india-stats/static/js/main.0d508d33.chunk.js"
+  },
+  {
+    "revision": "065f225e0a50f20a474b",
+    "url": "/covid19-india-stats/static/js/runtime-main.582d95d2.js"
+  },
+  {
+    "revision": "22f5a4e90741d447dccf591973434802",
+    "url": "/covid19-india-stats/static/media/logo.22f5a4e9.png"
+  }
+]);
